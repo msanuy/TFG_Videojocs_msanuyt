b5961aae822629e586321f1b587a776e2ccc18c9
msanuyt <80323219+msanuyt@users.noreply.github.com>
2021-04-04 11:54:38 +0200

    04/04
    Actualització amb el prototip compilat i diversos canvis d'aquesta setmana i l'anterior.

---
+++ msanuyt_prototip/Assets/FPS/Scripts/UI/QuitGame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Quits the game
+
+    void Update() => Application.Quit();
+}
